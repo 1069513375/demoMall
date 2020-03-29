819722acddecf4c8bdf4513e206d6648bf779c5f
1069513375 <1069513375@qq.com>
2020-03-29 15:34:35 +0800

    request modified

--- src/network/request.js
+++ src/network/request.js
@@ -3,9 +3,7 @@ import axios from 'axios'
 export function request(config) {
     //1.create an axios's instance(实例)
     const instance = axios.create({
-        // baseURL : "http://152.136.185.210:8000/api/h3",
-        // baseURL :  "http://123.207.32.32:8000/api/h3",
-        baseURL : "http://106.54.54.237:8000/api/h3",
+        baseURL : "http://xxxxx:8000/api/h3",
         timeout: 5000
     })
     //2. the intercreptors of request 请求拦截
